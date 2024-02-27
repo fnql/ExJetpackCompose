4cf5d457e4315d233c5078c6c8dc07aa6a1652fa
woogi <dbwhddnr00@naver.com>
2024-02-27 23:13:44 +0900

    composable 사용 수정

--- app/src/main/java/com/example/exjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/exjetpackcompose/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.exjetpackcompose
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            BasicsCodelabTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
     }
 }
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BasicsCodelabTheme {
+        Greeting("Android")
+    }
